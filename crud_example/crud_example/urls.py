8984fcc2fff9fdcc2f87cae16b3001d4fe0fe5f9
Cristian-jimenez-creator <cristian.jimenezbast@campusucc.edu.co>
2025-11-25 10:49:57 -0500

    Primer commit del proyecto Django

---
+++ crud_example/crud_example/urls.py
@@ -0,0 +1,8 @@
+# proyecto/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('estudiantes/', include('app_estudiantes.urls')),
+]
